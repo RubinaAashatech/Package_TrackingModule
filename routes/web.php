327731a4df2bc75912ab3da5840e408f8b671718
RubinaKhanal7 <rubikhanal8@gmail.com>
2024-07-29 12:11:45 +0545

    chnages

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+     ->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::get('/register', [RegisteredUserController::class, 'create'])
+     ->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
+     ->name('password.request');
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+     ->name('password.email');
+Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+     ->name('password.reset');
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+     ->name('password.update');
+Route::get('/verify-email/{id}/{hash}', [EmailVerificationPromptController::class, '__invoke'])
+     ->name('verification.verify');
+Route::get('/verify-email', [EmailVerificationNotificationController::class, 'create'])
+     ->middleware('auth')
+     ->name('verification.notice');
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+     ->middleware('auth')
+     ->name('verification.send');
+Route::post('/user/confirm-password', [ConfirmablePasswordController::class, 'store'])
+     ->middleware('auth')
+     ->name('password.confirm');
 
 Route::get('/', function () {
     return view('welcome');
